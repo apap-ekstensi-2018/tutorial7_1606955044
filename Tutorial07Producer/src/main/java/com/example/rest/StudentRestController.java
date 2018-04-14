dc0652c1247bc38ab33d12ea8bc9bb6ac88a8e89
Shinta Kusuma Wardhani <shintawardhani@shintas.local>
2018-04-14 11:19:24 +0700

    tutorial 07 v2

--- Tutorial07Producer/src/main/java/com/example/rest/StudentRestController.java
+++ Tutorial07Producer/src/main/java/com/example/rest/StudentRestController.java
@@ -1,5 +1,7 @@
 package com.example.rest;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,4 +20,10 @@ public class StudentRestController {
 	 StudentModel  student = studentService . selectStudent  ( npm );
 	 return  student; }
 
-}
+
+	@RequestMapping ("/student/viewall")
+		public List<StudentModel> viewall() {
+			List<StudentModel> student = studentService.selectAllStudents();
+			return student;
+		}
+	}
